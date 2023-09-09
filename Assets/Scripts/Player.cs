73317bc825e3d8da74b0c507bca15e3a971c88a5
cnkbat <cenkemirbat@gmail.com>
2023-09-09 15:57:29 +0300

    sens update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -56,6 +56,12 @@ public class Player : MonoBehaviour
     [SerializeField] List<GameObject> WSPositiveList,WSNegativeList;
     public bool positiveTurn, negativeTurn;
 
+
+    [Header("Testing Input")]
+    [SerializeField] private float maxDisplacement = 0.2f;
+    [SerializeField] private float maxPositionX = 2f;
+    private Vector2 _anchorPosition;
+
     private void Awake() 
     {
         if(instance == null)
@@ -87,10 +93,67 @@ public class Player : MonoBehaviour
         {
             if(!knockbacked)
             {
-                MoveCharacter();
+              //  MoveCharacter();
+                var inputX = GetInput();
+
+                var displacementX = GetDisplacement(inputX);
+
+                displacementX = SmoothOutDisplacement(displacementX);
+
+                var newPosition = GetNewLocalPosition(displacementX);
+
+                newPosition = GetLimitedLocalPosition(newPosition);
+
+                transform.localPosition = newPosition;
             }
         }
     }
+// TESTİNG INPUT
+
+ private Vector3 GetLimitedLocalPosition(Vector3 position)
+        {
+            position.x = Mathf.Clamp(position.x, -maxPositionX, maxPositionX);
+            return position;
+        }
+        private Vector3 GetNewLocalPosition(float displacementX)
+        {
+            var lastPosition = transform.localPosition;
+            var newPositionX = lastPosition.x + displacementX;
+            var newPosition = new Vector3(newPositionX, lastPosition.y, lastPosition.z +  forwardMoveSpeed *Time.deltaTime );
+            return newPosition;
+        }
+        private float GetInput()
+        {
+            var inputX = 0f;
+            if (Input.GetMouseButtonDown(0))
+            {
+                _anchorPosition = Input.mousePosition;
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                inputX = (Input.mousePosition.x - _anchorPosition.x);
+                _anchorPosition = Input.mousePosition;
+            }
+            return inputX;
+        }
+        private float GetDisplacement(float inputX)
+        {
+            var displacementX = 0f;
+            displacementX = inputX * Time.deltaTime;
+            return displacementX;
+        }
+        private float SmoothOutDisplacement(float displacementX)
+        {
+            return Mathf.Clamp(displacementX, -maxDisplacement, maxDisplacement);
+        }
+
+
+
+
+
+/// <summary>
+/// /TESTİNG İNPUT
+/// </summary>
 
     private void OnTriggerEnter(Collider other) 
     {   
